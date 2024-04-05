76fd4bdebb5ec3041a9589a7b70bb926b7da237a
Rem Lamb <rem.lamb@outlook.fr>
2024-04-05 13:58:17 +0200

    music =! sound

--- main.cpp
+++ main.cpp
@@ -9,37 +9,47 @@
 
 #include "Image.h"
 
-static constexpr int screenWidth = 800;
-static constexpr int screenHeight = 650;
+static constexpr int screenWidth = 1080;
+static constexpr int screenHeight = 720;
 
 class Renderer {
  public:
-  Sound music;
+  Sound sound;
+  Music music;
 
+  ImageCustom img;
   ImageCustom omniman;
   float customScale = 0.0f;
   Vector2 center = {screenWidth * 0.5f, screenHeight * 0.5f};
 
   void Init() {
+    img.Setup("data/2222.png", 1.0f, Pivot::Center);
     omniman.Setup("data/omniman.png", 1.8f, Pivot::Center);
     InitAudioDevice();
-    music = LoadSound("data/music.wav");
-    PlaySound(music);
+    sound = LoadSound("data/walkman.wav");
+    music = LoadMusicStream("data/music.wav");
+    music.looping = true;
+    PlaySound(sound);
+
+    PlayMusicStream(music);
   }
 
   void Deinit() {
     omniman.TearDown();
-    UnloadSound(music);  // Unload sound
-
+    img.TearDown();
+    UnloadSound(sound);  // Unload sound
+    UnloadMusicStream(music);
     CloseAudioDevice();
     CloseWindow();
   }
 
   void Loop(void) {
+    UpdateMusicStream(music);
     BeginDrawing();
     {
       ClearBackground(BLACK);
       customScale += 0.01f;
+      img.Draw(Vector2{screenWidth * 0.8f, screenHeight * 0.5f});
       omniman.Draw(center, customScale);
 
       DrawText("Raylib sample !!!", 290, 100, 30, WHITE);
@@ -53,31 +63,25 @@ void UpdateDrawFrame(void* renderer) {
   static_cast<Renderer*>(renderer)->Loop();
 }
 
-ImageCustom img;
-
 int main() {
-  InitWindow(screenWidth, screenHeight, "WakfuRaylibsssss");
+  InitWindow(screenWidth, screenHeight, "Online Game");
 
   Renderer renderer;
   renderer.Init();
-  img.Setup("data/2222.png", 1.0f, Pivot::Center);
 
 #ifdef PLATFORM_WEB
   emscripten_set_main_loop_arg(UpdateDrawFrame, &renderer, 0, 1);
 
 #else
-  SetTargetFPS(60);
+  SetTargetFPS(100);
 
   while (!WindowShouldClose()) {
-    // UpdateDrawFrame();
     renderer.Loop();
-    img.Draw(Vector2{screenWidth * 0.8f, screenHeight * 0.5f});
   }
 
 #endif
 
   renderer.Deinit();
-  img.TearDown();
 
   return EXIT_SUCCESS;
 }
